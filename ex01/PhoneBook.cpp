581f0e03d50a8dd85b0029f9a073d47e45d71ea0
xhsle <mhammedboukelalen@gmail.com>
2023-11-04 18:02:53 +0000

    %8 fix

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: xshel <xshel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 14:25:44 by m-boukel          #+#    #+#             */
-/*   Updated: 2023/11/03 20:19:15 by xshel            ###   ########.fr       */
+/*   Updated: 2023/11/04 18:01:13 by xshel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,53 +38,52 @@ int    PhoneBook::is_number(std::string str, std::string input)
 int    PhoneBook::add_contact()
 {
     std::string input;
-    
-    if (this->nb_contacts == 8)
-        this->nb_contacts = 0;
+    int current_index;
+    current_index = this->nb_contacts % 8;
     std::cout << nb_contacts << "-----------"<< std::endl;
-    this->contacts[nb_contacts].set_index(nb_contacts);
+    this->contacts[current_index].set_index(current_index);
     std::cout << "Enter first name: ";
     if (!(std::getline(std::cin, input)))
         return 1;
-    if (input == "")
-    {
-        std::cout << "No first name entered" << std::endl;
-        return 1;
-    }
-    this->contacts[nb_contacts].set_first_name(input);
+    // if (input == "")
+    // {
+    //     std::cout << "No first name entered" << std::endl;
+    //     return 1;
+    // }
+    this->contacts[current_index].set_first_name(input);
     std::cout << "Enter last name: ";
     if (!(std::getline(std::cin, input)))
         return 1;
-    if (input == "")
-    {
-        std::cout << "No last name entered" << std::endl;
-        return 1;
-    }
-    this->contacts[nb_contacts].set_last_name(input);
+    // if (input == "")
+    // {
+    //     std::cout << "No last name entered" << std::endl;
+    //     return 1;
+    // }
+    this->contacts[current_index].set_last_name(input);
     std::cout << "Enter nickname: ";
     if (!(std::getline(std::cin, input)))
         return 1;
-    if (input == "")
-    {
-        std::cout << "No nickname entered" << std::endl;
-        return 1;
-    }
-    this->contacts[nb_contacts].set_nickname(input);
+    // if (input == "")
+    // {
+    //     std::cout << "No nickname entered" << std::endl;
+    //     return 1;
+    // }
+    this->contacts[current_index].set_nickname(input);
     std::cout << "Enter phone number: ";
     if (!(std::getline(std::cin, input)))
         return 1;
-    if (is_number("phone number", input) == 1)
-        return 1;
-    this->contacts[nb_contacts].set_phone_number(input);
+    // if (is_number("phone number", input) == 1)
+    //     return 1;
+    this->contacts[current_index].set_phone_number(input);
     std::cout << "Enter darkest secret : ";
     if (!(std::getline(std::cin, input)))
         return 1;
-    if (input == "")
-    {
-        std::cout << "No darkest secret entered" << std::endl;
-        return 1;
-    }
-    this->contacts[nb_contacts].set_darkest_secret(input);
+    // if (input == "")
+    // {
+    //     std::cout << "No darkest secret entered" << std::endl;
+    //     return 1;
+    // }
+    this->contacts[current_index].set_darkest_secret(input);
     this->nb_contacts++;
     return 0;
 }
@@ -97,7 +96,8 @@ void    PhoneBook::search_contact(PhoneBook phonebook)
     int arr[8];
 
     i = 0;
-    while (i < phonebook.nb_contacts)
+    int ret_count = phonebook.nb_contacts > 8 ? 8 : phonebook.nb_contacts;
+    while (i < ret_count)
     {
         arr[i] = i;
         std::cout << std::setw(8) << arr[i] <<".|";
@@ -127,9 +127,9 @@ void    PhoneBook::search_contact(PhoneBook phonebook)
     if (!(std::getline(std::cin, input)))
         return ;
     i = 0;
-    while(i < phonebook.nb_contacts)
+    while(i < ret_count)
     {
-        if (i == phonebook.nb_contacts && atoi(input.c_str())!= arr[i])
+        if (atoi(input.c_str()) > 7 || atoi(input.c_str()) < 0)
         {
             std::cout << "No contact with this index" << std::endl;
             return ;
