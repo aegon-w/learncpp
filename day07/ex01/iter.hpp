921c35cfb31ef2958602a067f63d59d99588ce27
aegon-w <mhammedboukelalen@gmail.com>
2024-01-18 11:58:45 -0500

    push

--- day07/ex01/iter.hpp
+++ day07/ex01/iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xshel <xshel@student.42.fr>                +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 10:02:21 by xshel             #+#    #+#             */
-/*   Updated: 2023/12/20 10:02:48 by xshel            ###   ########.fr       */
+/*   Updated: 2024/01/18 11:58:00 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 
 #include <iostream>
 
-template <typename T>
+template <typename T, typename U>
 
-void iter(T *array, int length, void (*f)(T const &))
+void iter(T *array, int length, U f)
 {
+    if (!array || !f)
+        return;
     for (int i = 0; i < length; i++)
         f(array[i]);
 }
